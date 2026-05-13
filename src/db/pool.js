14b6bd8273b2f9fd9c9820b02be321de2c52dfc3
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-13 16:09:18 -0300

    style: use end of line export

--- src/db/pool.js
+++ src/db/pool.js
@@ -2,4 +2,6 @@ import { Pool } from 'pg';
 
 import { DATABASE_URL } from '#root/constants.js';
 
-export const pool = new Pool({ connectionString: DATABASE_URL });
+const pool = new Pool({ connectionString: DATABASE_URL });
+
+export { pool };
